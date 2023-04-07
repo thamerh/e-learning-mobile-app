5094a07178d482897aecd30a72f13740033e8a7c
thamerh <hamdithamer99@gmail.com>
2023-04-07 03:09:33 +0200

    feat: integrate router in server main

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,17 +1,30 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+import bodyParser from "body-parser";
 const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 
+try {
+    await db.authenticate();
+    console.log('Database Connected...');
+} catch (error) {
+    console.error(error);
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// //static Vedio Folder
+// app.use('/Videos', express.static('./Videos'));
 
+// app.use(express.json());
+app.use(router);
 
 // testing 
-app.get('/', (req, res) => {
-  res.json({ success: true, message: 'Welcome to backend zone!' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ success: true, message: 'Welcome to backend zone!' });
+// });
 
 app.listen(8000, () => {
     console.log('port is listening');
