e8bca91b1e9ec4eabc51755b007bb303faca3d8e
thamerh <hamdithamer99@gmail.com>
2023-04-09 10:18:37 +0200

    feat: create static data for arabic theme used in app

---
+++ Front-End/src/config/StaticData.js
@@ -0,0 +1,19 @@
+const data = {
+    infractions: "المخالفات و العقوبات",
+    panneaux: "العلامات و الإشارات",
+    priorite: "الأولوية",
+    regles: "قواعد الجولان",
+    secourisme: "الإسعافات الأولية",
+    croisement: "المقاطعة و المجاوزة",
+    vehicules: "السّواق و العربات",
+    arret: "الوقوف و التّوقف",
+    mecanique: "الصّيانة",
+    themeMd: "نقل المواد الخطرة",
+    themeC: "صنف ج",
+    themeA: "صنف أ",
+    themeB: "صنف ب",
+    themeCe: "صنف ج+ه",
+    themeD: "صنف د",
+    themeD1: "صنف د1",
+  };
+  export default   data
